1675d8e5220ff813f8aa250b5be4a4ab17628a0b
AbhishekBharati <bharatiabhishek30@gmail.com>
2026-08-04 07:33:11 +0530

    Chore: Initial Spring Boot Scaffolding

---
+++ src/test/java/com/tech_eaze/hermes/HermesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech_eaze.hermes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HermesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
