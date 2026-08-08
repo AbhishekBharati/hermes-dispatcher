075c037cec41325c3a7a6add5e33f9061362121b
AbhishekBharati <bharatiabhishek30@gmail.com>
2026-08-08 16:22:16 +0530

    feat: implement NotificationConsumerService to orchestrate dynamic provider execution

---
+++ src/main/java/com/tech_eaze/hermes/service/NotificationConsumerService.java
@@ -0,0 +1,33 @@
+package com.tech_eaze.hermes.service;
+
+import com.tech_eaze.hermes.domain.ChannelConfiguration;
+import com.tech_eaze.hermes.domain.NotificationLog;
+import com.tech_eaze.hermes.repository.ChannelConfigurationRepository;
+import com.tech_eaze.hermes.service.channel.NotificationChannel;
+import com.tech_eaze.hermes.service.factory.ProviderFactory;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class NotificationConsumerService {
+    private final ChannelConfigurationRepository configRepository;
+    private final ProviderFactory providerFactory;
+
+    public void processNotification(NotificationLog notificationLog){
+        log.info("Porcessing notification ID: {}", notificationLog.getNotificationId());
+        ChannelConfiguration config = configRepository.findById(notificationLog.getChannel())
+                .orElseThrow(() -> new IllegalStateException("No Dashboard configuration found for channel: " + notificationLog.getChannel()));
+
+        NotificationChannel provider = providerFactory.getProvider(config.getActiveProvider());
+        log.info("Routing notification through {} provider", provider.getProviderCode());
+        boolean isSuccess = provider.send(notificationLog.getPayload(), config.getCredentials());
+        if(isSuccess){
+            log.info("Notification {} successfully processed", notificationLog.getNotificationId());
+        } else {
+            log.error("Notification {} failed to process", notificationLog.getNotificationId());
+        }
+    }
+}
